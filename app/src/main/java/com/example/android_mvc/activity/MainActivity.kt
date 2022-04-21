0b8ba97a109655eb5a050946196a5acb3903d9ec
khurshid88 <shamsun.com@gmail.com>
2022-04-21 09:01:33 +0500

    Initial Commit

---
+++ app/src/main/java/com/example/android_mvc/activity/MainActivity.kt
@@ -0,0 +1,78 @@
+package com.example.android_mvc.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.android_advanced_kotlin.activity.model.Post
+import com.example.android_mvc.R
+import com.example.android_mvc.adapter.PostAdapter
+import com.example.android_mvc.network.RetrofitHttp
+import com.example.android_mvc.utils.Utils
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var recyclerView: RecyclerView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        initViews()
+    }
+
+    private fun initViews() {
+        recyclerView = findViewById(R.id.recyclerView)
+        recyclerView.setLayoutManager(GridLayoutManager(this, 1))
+
+        apiPostList()
+    }
+
+    fun refreshAdapter(posters: ArrayList<Post>) {
+        val adapter = PostAdapter(this, posters)
+        recyclerView.setAdapter(adapter)
+    }
+
+    private fun apiPostList() {
+        RetrofitHttp.postService.listPost().enqueue(object : Callback<ArrayList<Post>> {
+            override fun onResponse(
+                call: Call<ArrayList<Post>>,
+                response: Response<ArrayList<Post>>
+            ) {
+                refreshAdapter(response.body()!!)
+            }
+
+            override fun onFailure(call: Call<ArrayList<Post>>, t: Throwable) {
+
+            }
+        })
+    }
+
+    private fun apiPostDelete(post: Post) {
+        RetrofitHttp.postService.deletePost(post.id).enqueue(object : Callback<Post> {
+            override fun onResponse(call: Call<Post>, response: Response<Post>) {
+                apiPostList()
+            }
+
+            override fun onFailure(call: Call<Post>, t: Throwable) {
+
+            }
+        })
+    }
+
+    fun deletePostDialog(post: Post) {
+        val title = "Delete"
+        val body = "Do you want to delete?"
+        Utils.customDialog(this, title, body, object : Utils.DialogListener {
+            override fun onPositiveClick() {
+                apiPostDelete(post)
+            }
+
+            override fun onNegativeClick() {
+
+            }
+        })
+    }
+}
---
+++ app/src/main/java/com/example/android_mvc/adapter/PostAdapter.kt
@@ -0,0 +1,54 @@
+package com.example.android_mvc.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.RecyclerView
+import com.example.android_advanced_kotlin.activity.model.Post
+import com.example.android_mvc.R
+import com.example.android_mvc.activity.MainActivity
+
+class PostAdapter(var activity: MainActivity, var items: ArrayList<Post>) :
+    RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        val view: View =
+            LayoutInflater.from(parent.context).inflate(R.layout.item_poster_list, parent, false)
+        return PosterViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val post: Post = items[position]
+        if (holder is PosterViewHolder) {
+            val ll_poster = holder.ll_poster
+            val tv_title = holder.tv_title
+            val tv_body = holder.tv_body
+            tv_title.setText(post.title.toUpperCase())
+            tv_body.setText(post.body)
+
+            ll_poster.setOnLongClickListener {
+                activity.deletePostDialog(post)
+                false
+            }
+        }
+    }
+
+    inner class PosterViewHolder(var view: View) : RecyclerView.ViewHolder(view) {
+        var ll_poster: LinearLayout
+        var tv_title: TextView
+        var tv_body: TextView
+
+        init {
+            ll_poster = view.findViewById(R.id.ll_poster)
+            tv_title = view.findViewById(R.id.tv_title)
+            tv_body = view.findViewById(R.id.tv_body)
+        }
+    }
+}
